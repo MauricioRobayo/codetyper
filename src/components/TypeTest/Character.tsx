6683436f95104783621e321ac6be3deea033e42c
Mauricio Robayo <rfmajo@gmail.com>
2022-06-10 14:10:28 -0500

    remove unnecessary prop

--- src/components/TypeTest/Character.tsx
+++ src/components/TypeTest/Character.tsx
@@ -1,23 +1,17 @@
 import { memo } from "react";
 
 type CharProps = {
-  key: string | number;
   className: string;
   textChar: string;
   typedKey: string;
 };
 
 const Character = memo(function Char({
-  key,
   className,
   textChar,
   typedKey,
 }: CharProps) {
-  return (
-    <span key={key} className={className}>
-      {typedKey || textChar}
-    </span>
-  );
+  return <span className={className}>{typedKey || textChar}</span>;
 });
 
 export default Character;
