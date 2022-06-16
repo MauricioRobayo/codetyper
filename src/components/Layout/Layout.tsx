9d05418d9521ed33d209457f7d66396c4a919d0d
Mauricio Robayo <rfmajo@gmail.com>
2022-06-16 11:07:06 -0500

    fix AppShell layout with fixed footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,38 @@
+import { Anchor, AppShell, Center, Footer, Header, Title } from "@mantine/core";
+import Link from "next/link";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 export function Layout({ children }: LayoutProps) {
   return (
     <>
-      <header>Code Typer</header>
-      {children}
-      <footer>Code Typer</footer>
+      <AppShell
+        header={
+          <Header height="auto" p="md" sx={{ backgroundColor: "transparent" }}>
+            <Link href="/" passHref>
+              <Anchor>
+                <Title>Code Typer</Title>
+              </Anchor>
+            </Link>
+          </Header>
+        }
+        footer={
+          <Footer fixed height="auto" p="md">
+            <Center>
+              <div>
+                This is an{" "}
+                <Anchor href="https://github.com/MauricioRobayo/code-typer">
+                  open source
+                </Anchor>{" "}
+                project
+              </div>
+            </Center>
+          </Footer>
+        }
+      >
+        {children}
+      </AppShell>
     </>
   );
 }
