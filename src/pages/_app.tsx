12317c738d5891d20380633ae39adb87c913083d
Mauricio Robayo <rfmajo@gmail.com>
2022-06-21 10:29:37 -0500

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,15 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { MantineProvider } from "@mantine/core";
 import { Layout } from "../components/Layout";
+import { GoogleAnalytics, usePagesViews } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
+  usePagesViews();
+
   return (
     <>
+      <GoogleAnalytics />
       <QueryClientProvider client={queryClient}>
         <MantineProvider
           withGlobalStyles
