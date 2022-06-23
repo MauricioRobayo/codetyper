6496a96b1c51e65122f59e034a02dbb0142bd539
Mauricio Robayo <rfmajo@gmail.com>
2022-06-23 10:36:36 -0500

    show line number and total lines

--- src/components/TypeTest/TypeTest.tsx
+++ src/components/TypeTest/TypeTest.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from "react";
+import { Text } from "@mantine/core";
+import { useEffect, useMemo } from "react";
 import Character from "./Character";
 import {
   CharacterStatus,
@@ -26,45 +27,53 @@ export function TypingTest({ text, onFinish, classes }: TypingTestProps) {
     ? Math.max(0, currentLine - SCROLL_LINE + 1)
     : 0;
   const lastVisibleLine = text.split("\n").length - VISIBLE_LINES;
+  const totalNumberOfLines = textState?.[textState.length - 1]?.line ?? 0;
 
   if (!textState) {
     return null;
   }
 
   return (
-    <pre className={classes?.textArea ?? ""}>
-      {textState
-        .filter(
-          ({ line }) =>
-            line >= Math.min(lineOffset, lastVisibleLine) &&
-            line < SCROLL_LINE + lineOffset + VISIBLE_LINES / 2
-        )
-        .map(({ char, status, typedKey, displayChar, isActive }, index) => {
-          const displayedEndOfLine: string = isActive
-            ? `${displayChar}\n`
-            : {
-                error: `${typedKey}\n`,
-                idle: `\n`,
-                correct: `\n`,
-                corrected: "\n",
-              }[status ?? "idle"];
-          return (
-            <Character
-              key={index}
-              className={
-                classes && index <= currentIndex
-                  ? isActive
-                    ? classes["active"]
-                    : status
-                    ? classes[status]
+    <>
+      <pre className={classes?.textArea ?? ""}>
+        {textState
+          .filter(
+            ({ line }) =>
+              line >= Math.min(lineOffset, lastVisibleLine) &&
+              line < SCROLL_LINE + lineOffset + VISIBLE_LINES / 2
+          )
+          .map(({ char, status, typedKey, displayChar, isActive }, index) => {
+            const displayedEndOfLine: string = isActive
+              ? `${displayChar}\n`
+              : {
+                  error: `${typedKey}\n`,
+                  idle: `\n`,
+                  correct: `\n`,
+                  corrected: "\n",
+                }[status ?? "idle"];
+            return (
+              <Character
+                key={index}
+                className={
+                  classes && index <= currentIndex
+                    ? isActive
+                      ? classes["active"]
+                      : status
+                      ? classes[status]
+                      : ""
                     : ""
-                  : ""
-              }
-              textChar={char}
-              typedKey={char === "\n" ? displayedEndOfLine : typedKey}
-            />
-          );
-        })}
-    </pre>
+                }
+                textChar={char}
+                typedKey={char === "\n" ? displayedEndOfLine : typedKey}
+              />
+            );
+          })}
+      </pre>
+      {totalNumberOfLines > 1 && (
+        <Text>
+          Line {currentLine + 1} of {totalNumberOfLines + 1}
+        </Text>
+      )}
+    </>
   );
 }
