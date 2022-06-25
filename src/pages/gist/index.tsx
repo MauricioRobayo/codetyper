24ccc7169ae13daba06f14eb3fc4e0064abd266e
Mauricio Robayo <rfmajo@gmail.com>
2022-06-24 22:05:04 -0500

    style form on gist root

--- src/pages/gist/index.tsx
+++ src/pages/gist/index.tsx
@@ -1,3 +1,4 @@
+import { Center, Container } from "@mantine/core";
 import type { NextPage } from "next";
 import Head from "next/head";
 import GistForm from "../../components/GistForm";
@@ -10,7 +11,11 @@ const GistPage: NextPage = () => {
         <meta name="description" content="Code Typer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <GistForm />
+      <Container>
+        <Center>
+          <GistForm showHeader />
+        </Center>
+      </Container>
     </>
   );
 };
